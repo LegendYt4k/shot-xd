b85ef354a9cd7c2382344619c640b563b2ea8392
LegendYt4k <139982135+LegendYt4k@users.noreply.github.com>
2023-08-29 11:02:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 })
 
 app.listen(8080)
-const ffmpegCommand = 'ffmpeg -stream_loop -1 -re -i neffex.mp4 -stream_loop -1 -re -i https://stream-152.zeno.fm/ez4m4918n98uv -c:v libx264 -pix_fmt yuvj420p -maxrate 512k -preset ultrafast -r 12 -framerate 1 -g 48 -crf 23 -c:a aac -b:a 96k -ar 44100 -strict -2 -video_track_timescale 100 -b:v 500k -s 1280x720 -f flv rtmp://a.rtmp.youtube.com/live2/26q2-0s21-rhxk-pvdr-00e1';
+const ffmpegCommand = 'ffmpeg -stream_loop -1 -re -i k.mp4 -stream_loop -1 -re -i https://stream-152.zeno.fm/ez4m4918n98uv -c:v libx264 -pix_fmt yuvj420p -maxrate 512k -preset ultrafast -r 12 -framerate 1 -g 48 -crf 23 -c:a aac -b:a 96k -ar 44100 -strict -2 -video_track_timescale 100 -b:v 500k -s 1280x720 -f flv rtmp://a.rtmp.youtube.com/live2/26q2-0s21-rhxk-pvdr-00e1';
 
 const ffmpegProcess = spawn(ffmpegCommand, {
   shell: true,
